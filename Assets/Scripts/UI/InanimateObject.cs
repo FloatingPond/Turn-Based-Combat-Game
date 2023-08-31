01498aa96ebb9ce21744dd40aec944b66c31af98
MrResistance <Paul1208Gush@Gmail.com>
2023-08-31 16:51:51 +0100

    Added InanimateObject Script

---
+++ Assets/Scripts/UI/InanimateObject.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PG
+{
+    public class InanimateObject : MonoBehaviour, IInteractable, IDamageable
+    {
+        public float currentHealth, maxHealth;
+        void Start()
+        {
+            currentHealth = maxHealth;
+        }
+        public void Die()
+        {
+            
+        }
+
+        void IInteractable.OnClick()
+        {
+            
+        }
+
+        void IInteractable.OnHoverEnter()
+        {
+            RoundManager.Instance.unitTakingTurn.unitActions.myTarget = this.transform;
+        }
+
+        void IInteractable.OnHoverExit()
+        {
+            RoundManager.Instance.unitTakingTurn.unitActions.myTarget = null;
+        }
+
+        void IInteractable.OnHoverStay()
+        {
+            
+        }
+        public void TakeDamage(float damage)
+        {
+            currentHealth -= damage;
+            if (currentHealth <= 0) Die();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
